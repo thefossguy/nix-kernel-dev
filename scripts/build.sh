b0f5dd26434f19d4beb1e5209ed40da1114bb654
Pratham Patel <thefirst1322@gmail.com>
2024-03-18 17:01:12 +0530

    do not write to a log file when removing the kernel

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,9 @@
 set -x
 KERNEL_LOCALVERSION="-$(date +%Y.%m.%d.%H%M)"
 export KERNEL_LOCALVERSION
-"$(dirname "$0")/unwrapped-build-kernel-and-install.sh" "$@" 2>&1 | tee "build-$(make -s kernelversion)${KERNEL_LOCALVERSION}.log"
+
+if [ -z "${REMOVE_KERNEL}" ]; then
+    "$(dirname "$0")/unwrapped-build-kernel-and-install.sh" 2>&1 | tee "build-$(make -s kernelversion)${KERNEL_LOCALVERSION}.log"
+else
+    "$(dirname "$0")/unwrapped-build-kernel-and-install.sh"
+fi
