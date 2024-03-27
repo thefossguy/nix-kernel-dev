11a113db873e121ff092fdc5b4e8a5cd16a7f68e
Pratham Patel <prathampatel@thefossguy.com>
2024-03-27 19:33:12 +0530

    build: debian: fix wrong option to ignore missing build deps

--- scripts/debian-build-kernel-and-install.sh
+++ scripts/debian-build-kernel-and-install.sh
@@ -4,7 +4,7 @@
 set -xeu -o pipefail
 
 function build_kernel() {
-    sed -i 's/dpkg-buildpackage \\/dpkg-buildpackage -b \\/g' scripts/Makefile.package
+    sed -i 's/dpkg-buildpackage \\/dpkg-buildpackage -d \\/g' scripts/Makefile.package
     # shellcheck disable=SC2086
     make $MAX_PARALLEL_JOBS bindeb-pkg
 }
